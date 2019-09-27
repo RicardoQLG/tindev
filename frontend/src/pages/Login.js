154656e0ac2e9eca3d6021343b60f6a311e21335
Ricardo Godoy <rq.godoy@gmail.com>
2019-09-27 01:02:15 -0300

    Frontend alteado de submodulo para parte do projeto

---
+++ frontend/src/pages/Login.js
@@ -0,0 +1,38 @@
+import React, {useState} from 'react';
+import './Login.css';
+
+import logo from '../assets/logo.svg';
+
+import api from '../services/api';
+
+export default function Login ({ history }) {
+  const [username, setUsername] = useState('');
+
+  async function handleSubmit (event) {
+    event.preventDefault();
+
+    const apiResponse = await api.post('/devs', {
+      username
+    });
+
+    const { _id } = apiResponse.data;
+
+    history.push(`/dev/${_id}`);
+  }
+
+  return (
+    <div className="login-container">
+      <form onSubmit={handleSubmit}>
+        <img src={logo} alt="Tindev" />
+        <input
+          placeholder="Digite seu usuário no github"
+          value={username}
+          onChange={event => { setUsername (event.target.value); }}
+        />
+        <button type="submit">
+          Enviar
+        </button>
+      </form>
+    </div>
+  );
+}
